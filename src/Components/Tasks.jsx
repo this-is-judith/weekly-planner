4038e8466491af00f17edfadb87d87c36c0030c1
Judith Osei-Tete <jao92@cornell.edu>
2024-05-29 14:55:52 -0400

    structure tasks main and archive layout

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./tasks.css";
 
 function Tasks() {
-  return <div>This is Tasks component</div>;
+  return (
+    <>
+      <div className="tasks-parent">
+        <div className="tasks-component tasks-main"></div>
+        <div className="tasks-component tasks-archive"></div>
+      </div>
+    </>
+  );
 }
 
 export default Tasks;
