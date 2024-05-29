b27faf448cce261d8da39b095c441f03c8c9b6a5
Judith Osei-Tete <jao92@cornell.edu>
2024-05-29 14:48:15 -0400

    solve height layout issue

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
   return (
     <>
       <div className="home-container">
-        <div className="main-container tasks-container">
+        <div className="section-container main-container tasks-container">
           <Tasks />
         </div>
 
-        <div className="main-container calendar-and-notes-container">
-          <div className="calendar-container">
+        <div className="section-container calendar-and-notes-container">
+          <div className="main-container calendar-container">
             <Calendar />
           </div>
 
-          <div className="notes-container">
+          <div className="main-container notes-container">
             <Notes />
           </div>
         </div>
