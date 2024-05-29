fd18f2a6647f6deedf355cdaa809acc99e466fe2
Judith Osei-Tete <jao92@cornell.edu>
2024-05-29 13:29:11 -0400

    add textarea and archive for notes component

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "./notes.css";
 
 function Notes() {
-  return <div>This is Notes component</div>;
+  // State to manage the content of the textarea
+  const [note, setNote] = useState("");
+
+  // Function to handle changes in the textarea
+  const handleNoteChange = (event) => {
+    setNote(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="notes-parent">
+        <div className="notes-component notes-textarea">
+          <textarea
+            className="textarea"
+            value={note}
+            onChange={handleNoteChange}
+            placeholder="Write your notes here..."
+          />
+        </div>
+
+        <div className="notes-component notes-archive">Notes Archive</div>
+      </div>
+    </>
+  );
 }
 
 export default Notes;
