050d87a6fe09362e8139d929e6528254b48053cf
Judith Osei-Tete <jao92@cornell.edu>
2024-05-30 22:19:27 -0400

    add header components, logo and name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,15 +7,18 @@ function Header() {
     <>
       <div className="header-parent">
         <div className="header-component header-logo">
-          <RiRhythmFill />
+          <RiRhythmFill size="30" />
         </div>
 
-        <div className="header-component header-name"></div>
-        <div className="header-component"></div>
-        <div className="header-component"></div>
-        <div className="header-component"></div>
-        <div className="header-component"></div>
-        <div className="header-component"></div>
+        <div className="header-component header-name">
+          <h1>Simple Rhythm</h1>
+        </div>
+
+        <div className="header-component header-calendar-month-year"></div>
+        <div className="header-component header-change-week"></div>
+        <div className="header-component header-search"></div>
+        <div className="header-component header-settings"></div>
+        <div className="header-component header-user"></div>
       </div>
     </>
   );
